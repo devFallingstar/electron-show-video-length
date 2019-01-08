495a87540f4de8249b9cb9f2bfb09882b508937c
DevFallingstar <devfallingstar@gmail.com>
2019-01-08 15:21:37 +0900

    Basic Window

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const electron = require('electron');
 
-const { app } = electron; // Overall electron app process property
+const { app, BrowserWindow } = electron; // Overall electron app process property
 
 app.on('ready', () => {
-  console.log('App is now ready');
+  const mainWindow = new BrowserWindow({}); // Pass empty object with some configurations
+  mainWindow.loadURL(`file://${__dirname}/index.html`); // ${__dirname} means current working directory
 });
