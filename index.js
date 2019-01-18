7110ccee6f9dfb2bb19242b6d2f189d681a18a0a
DevFallingstar <devfallingstar@gmail.com>
2019-01-18 19:08:49 +0900

    Add basic ipcMain on Electron-App side source code

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron; // Overall electron app process property
+const { app, BrowserWindow, ipcMain } = electron; // Overall electron app process property
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({}); // Pass empty object with some configurations
   mainWindow.loadURL(`file://${__dirname}/index.html`); // ${__dirname} means current working directory
+});
+
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    console.log('Video duration is: ', metadata.format.duration);
+  })
 });
